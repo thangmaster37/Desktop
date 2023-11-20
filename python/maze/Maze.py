1a22422562bab8d216fddb9645c9e618d051bebd
Hideyoshi <imlda1053@gmail.com>
2023-11-20 20:11:23 +0700

    Fix

--- python/maze/Maze.py
+++ python/maze/Maze.py
@@ -1,26 +1,30 @@
-# Author __Pham Ngoc Hai - Harito__
-
-# Home work week ...
-# all prefix sum GPU 
-# sort array vs GPU 
 import numpy as np
 
+
 class Maze:
-    # Create a maze with rows, cols and obstacle_prob para or create a Maze with no maze info 
-    def __init__(self, rows=None, cols=None, obstacle_prob=None):
-        if rows == None or cols == None or obstacle_prob == None:
-            self.maze = None
-            return
-        random_values = np.random.rand(rows, cols)
-        self.maze = np.full((rows, cols), False, dtype=bool)
+    # Create a maze with rows, cols and obstacle_prob para or create a Maze with no maze info
+    def __init__(self, shape: tuple, obstacle_prob=0.0, maze=None):
+        self.rows = shape[0]
+        self.cols = shape[1]
+        self.maze = maze
+        if maze is None:
+            self.__generate__(obstacle_prob)
+
+    def __generate__(self, obstacle_prob):
+        random_values = np.random.rand(self.rows, self.cols)
+        self.maze = np.full((self.rows, self.cols), False, dtype=bool)
         self.maze[random_values < obstacle_prob] = True
+
+    @classmethod
+    def array(cls, array):
+        maze = np.array(array, dtype=bool)
+        return cls(maze.shape, maze=maze)
 
     # Change maze follow by probability and check the updated maze can have path to go or not
     def change_maze(self, prob_false_to_true, prob_true_to_false):
         is_dead_maze = 0
-        shape = self.maze.shape
-        for i in range(shape[0]):
-            for j in range(shape[1]):
+        for i in range(self.rows):
+            for j in range(self.cols):
                 random_value = np.random.rand()
                 if not self.maze[i, j]:
                     if random_value < prob_false_to_true:
@@ -33,48 +37,55 @@ class Maze:
         return is_dead_maze == 0
 
     # Return a clone maze (type: Maze)
-    def clone_maze(self):
-        new_maze = Maze()
-        new_maze.maze = np.copy(self.maze)
-        return new_maze
+    def copy(self):
+        return Maze((self.rows, self.cols), maze=np.copy(self.maze))
 
     # Check the current position can go throw or not
-    def can_pass(self, current:tuple):
+    def is_valid(self, current: tuple):
         return current[0] >= 0 and current[1] >= 0 \
-            and current[0] < self.maze.shape[0] \
-            and current[1] < self.maze.shape[1] \
+            and current[0] < self.rows\
+            and current[1] < self.cols \
             and not self.maze[current[0], current[1]]
 
     # Return a list of neighbors can reach of the current position
-    def neighbors(self, current:tuple):
+    def get_neighbors(self, current: tuple):
         neighbors = []
         directions = ((-1, 0), (0, 1), (1, 0), (0, -1))
         for move in directions:
-            neighbor = tuple(current[0] + move[0], current[1] + move[1])
-            if self.can_pass(neighbor):
+            neighbor = (current[0] + move[0], current[1] + move[1])
+            if self.is_valid(neighbor):
                 neighbors.append(neighbor)
         return neighbors
 
-    def solve(self, ) 
+    def __str__(self):
+        return self.maze.__str__()
+
 
 def main():
     # Sử dụng lớp Maze
-    rows = 5  # Số hàng 
+    rows = 5  # Số hàng
     cols = 5  # Số cột
     obstacle_prob = 0.3  # Xác suất ô bị chặn
 
-    maze_instance = Maze(rows, cols, obstacle_prob)
+    maze_instance = Maze((rows, cols), obstacle_prob)
 
-    # Gọi các phương thức của lớp Maze
+    # # Gọi các phương thức của lớp Maze
     print("Initial Maze:")
-    print(maze_instance.maze)
+    print(maze_instance)
+
+    maze2 = Maze.array([[True, True, False],
+                        [True, True, False],
+                        [True, True, False],
+                        [True, True, False],
+                        [True, True, False]])
+    print(maze2)
 
     is_dead_maze = maze_instance.change_maze(0.5, 0)
     print("Changed Maze:")
     print(maze_instance.maze)
     print("Is Dead Maze:", is_dead_maze)
 
-    cloned_maze = maze_instance.clone_maze()
+    cloned_maze = maze_instance.copy()
     print("Cloned Maze:")
     print(cloned_maze.maze)
 
@@ -82,6 +93,7 @@ def main():
     print("Change cloned Maze:")
     print(cloned_maze.maze)
     print("Is Dead Maze:", is_dead_maze)
+
 
 if __name__ == "__main__":
     main()

3bee733cd5ac3cd600e16376a014734d4b991971
Harito97 <harito9703@gmail.com>
2023-11-20 21:57:06 +0700

    final update design structure

--- python/stmaze/ACO.py
+++ python/stmaze/ACO.py
@@ -1,16 +1,15 @@
 # Author __Luong Duc Anh__
 from maze import Maze
 
-class ACO():
-    def aco_algo(maze: Maze):
-        # ...
-        # return a tuple of (aco_path, pheromone_map)
-        return (0, 0)
-    
-    def aco_path(maze: Maze):
-        # [start, (x,y), (a, b), ..., end] - numpy array <=> ACO.aco_algo()[0]
-        return aco_algo(maze)[0]     
-    
-    def map(maze: Maze): 
-        # a dictionary about pheromone map <=> ACO.aco_algo()[1]
-        return 
+def aco_algo(maze: Maze):
+    # ...
+    # return a tuple of (aco_path, pheromone_map)
+    return (0, 0)
+
+def aco_path(maze: Maze):
+    # [start, (x,y), (a, b), ..., end] - numpy array <=> ACO.aco_algo()[0]
+    return aco_algo(maze)[0]     
+
+def map(maze: Maze): 
+    # a dictionary about pheromone map <=> ACO.aco_algo()[1]
+    return aco_algo(maze)[1]     
--- python/stmaze/AStar.py
+++ python/stmaze/AStar.py
@@ -1,12 +1,12 @@
 # Author __Tran Minh Duc__
 from maze import Maze
 
-class AStar():
-    def astar_path(maze: Maze):
-        return None # [start, (x,y), (a, b), ..., end] - numpy array
+
+def astar_path(maze: Maze):
+    return None # [start, (x,y), (a, b), ..., end] - numpy array
+
+def map(maze: Maze): 
+    astar_path = astar_path(maze)
+    # transform numpy array - A* path: self.astar_path to a dict
     
-    def map(maze: Maze): 
-        astar_path = astar_path(maze)
-        # transform numpy array - A* path: self.astar_path to a dict
-        
-        return None # a dictionary{start: (x, y), (x, y): (a, b), (a, b): ..., ...., ...: end}
+    return None # a dictionary{start: (x, y), (x, y): (a, b), (a, b): ..., ...., ...: end}

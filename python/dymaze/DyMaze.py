eea86921a994e53a65f0b14f4ece31df6dbd38b3
Harito97 <harito9703@gmail.com>
2023-11-21 12:27:25 +0700

    update maze structure

--- python/dymaze/DyMaze.py
+++ python/dymaze/DyMaze.py
@@ -8,5 +8,21 @@ sys.path.append(os.path.abspath(package_path))
 from maze.Maze import Maze
 
 class DyMaze(Maze):
-    
+    # Step 0. Prepare maze
+    # Create maze
+    # Choose start point, end point
+    # Prepare n - objects will start from start point to end point
+
+    # Step 1. Prepare astar_map - the astar optimize path and aco_map - the pheromone map
+    # astar_map
+    # aco_map
+
+    # Step 2. Moving objects to end point & Update maze at the same time
+    # Write the move of ech object and each stage of maze when it change
+    # (Every one step of each object)
+    # Stop condition: 
+    #   |___N - objects come end point success
+    #   |___Maze have no more path to go
+    #       => Number object come end point + Number object stuck in maze == N
+    # Read the data be write and display
     pass

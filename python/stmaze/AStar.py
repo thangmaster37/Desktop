10ba10b5aed51dc30b405e51692f313d1a45b44f
TranMinhDuc <92939191+TranMinhDuc190103@users.noreply.github.com>
2023-11-22 10:08:33 +0700

    Update AStar.py

--- python/stmaze/AStar.py
+++ python/stmaze/AStar.py
@@ -6,16 +6,6 @@ package_path = "./python"
 sys.path.append(os.path.abspath(package_path))
 
 from maze.Maze import Maze
-
-class AStar():
-    def __init__(self, maze: Maze# Author __Tran Minh Duc__
-import sys, os
-# Get the parent directory
-package_path = "./python"
-# Add the parent directory to sys.path
-sys.path.append(os.path.abspath(package_path))
-
-from maze.Maze import Maze
 from queue import PriorityQueue
 class AStar():
     def __init__(self, maze: Maze):
@@ -63,22 +53,6 @@ class AStar():
                         open_set.add(neighbor)
             
             
-        # transform numpy array - A* path: astar_path to a dict
-        # ... tasks: Duc complete the code in here
-        return astar_path # [start, (x,y), (a, b), ..., end] - numpy array
-
-    def map(self):  
-        return self.astar_map # a dictionary{start: (x, y), (x, y): (a, b), (a, b): ..., ...., ...: end}
-        self.maze = maze
-        self.astar_map = {}
-
-    def heuristic():
-        # ... tasks: Duc complete the code in here
-        pass
-
-    def astar(self, start: tuple, end: tuple):
-        self.astar_map = {}     # restart the value of self.astar_map
-        astar_path = None
         # transform numpy array - A* path: astar_path to a dict
         # ... tasks: Duc complete the code in here
         return astar_path # [start, (x,y), (a, b), ..., end] - numpy array

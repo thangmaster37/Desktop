132264bfe20b5d72f6adb408f31ad8e938f1a30c
TranMinhDuc <92939191+TranMinhDuc190103@users.noreply.github.com>
2023-11-22 11:27:00 +0700

    Update AStar.py

--- python/stmaze/AStar.py
+++ python/stmaze/AStar.py
@@ -12,21 +12,21 @@ import numpy as np
 class AStar():
     def __init__(self, maze: Maze):
         self.maze = maze
-        self.astar_map = []
+        self.astar_map = {}
 
     def astar(self, start: tuple, end: tuple):
-        heuristic = lambda x, y, z, t: abs(x-z) + abs(y-t)   
+        heuristic = lambda x, y: abs(x[0] - y[0]) + abs(x[1] - y[1]) 
         count = 0
         open_queue = PriorityQueue()
-        open_set = [start]
-        node_path = []
-        astar_path_list = []
+        open_set = {start}
+        node_path = {}
+        self.astar_map = {}
         open_queue.put((0, count, start))
 
-        g_score = [[float("inf") for spot in row] for row in self.maze.maze]
-        g_score[start[0]][start[1]] = 0
-        f_score = [[float('inf') for spot in row] for row in self.maze.maze]
-        f_score[start[0]][start[1]] = heuristic(start[0], start[1], end[0], end[1])
+        g_score = np.full(self.maze.rows, self.maze.cols, float('inf'))
+        g_score[start[0], start[1]] = 0
+        f_score = np.full(self.maze.rows, self.maze.cols, float('inf'))
+        f_score[start[0], start[1]] = heuristic(start, end)
         
         while not open_queue.empty():
             #... tasks: Duc complete the code in here
@@ -34,30 +34,29 @@ class AStar():
             open_set.remove(current_node)
             if current_node == end:
                 while current_node!= start:
-                    astar_path_list.append(current_node)
+                    self.astar_map.add(current_node)
                     current_node = open_set[current_node]
-                astar_path_list.append(start)
-                astar_path_list.reverse()
+                self.astar_map.append(start)
+                self.astar_map.reverse()
                 break
             
-            neighbors = self.maze.get_neighbors()
+            neighbors = self.maze.get_neighbors(current_node)
             for neighbor in neighbors:
-                temp_g_score = g_score[current_node[0]][current_node[1]] + 1
-                if temp_g_score < g_score[neighbor[0]][neighbor[1]]:
+                temp_g_score = g_score[current_node[0], current_node[1]] + 1
+                if temp_g_score < g_score[neighbor[0], neighbor[1]]:
                     node_path[neighbor] = current_node
-                    g_score[neighbor[0]][neighbor[1]] = temp_g_score
-                    f_score[neighbor[0]][neighbor[1]] = temp_g_score + heuristic(neighbor, end)
+                    g_score[neighbor[0], neighbor[1]] = temp_g_score
+                    f_score[neighbor[0], neighbor[1]] = temp_g_score + heuristic(neighbor, end)
                     if neighbor not in open_set:
                         count +=1 
-                        open_queue.put(f_score[neighbor[0]][neighbor[1]], count, neighbor)
-                        open_set.add(neighbor)
+                        open_queue.put(f_score[neighbor[0], neighbor[1]], count, neighbor)
+                        open_set.append(neighbor)
                         
         # transform numpy array - A* path: astar_path to a dict
-        self.astar_path = []
-        for node in astar_path_list:
-            self.astar_path[node] = node_path[node]
         # ... tasks: Duc complete the code in here
-        return self.astar_path # [start, (x,y), (a, b), ..., end] - numpy array
+        
+        
+        return astar_path # [start, (x,y), (a, b), ..., end] - numpy array
 
     def map(self):  
         return self.astar_map # a dictionary[start: (x, y), (x, y): (a, b), (a, b): ..., ...., ...: end]
@@ -70,4 +69,5 @@ if __name__ == "__main__":
          [False, True, True, True, False],
          [False, False, False, False, False]])
     # print(my_maze)
-    # astar = AStar(my_maze)
+    astar = AStar(my_maze)
+    # print(astar.astar((0,0), (4,4)))
